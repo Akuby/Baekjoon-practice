3cd073af1547112c457472efa8ca4639265c4612
Akuby <96938229+Akuby@users.noreply.github.com>
2022-01-26 17:35:11 +0900

    if문 연습

--- Baekjoon/14681.js
+++ Baekjoon/14681.js
@@ -1,4 +1,22 @@
-var input = parseInt(require('fs').readFileSync('/dev/stdin').toString().trim().split('\n'));
+const readline = require('readline'); 
+const rl = readline.createInterface({ 
+    input: process.stdin, 
+    output: process.stdout
+}); 
 
+let input = []; 
+rl.on('line', function (line) { 
+    input.push(Number(line)); 
+}).on('close', function () { 
+    
 const a = input[0];
 const b = input[1];
+if(a>0){
+    b>0 ? console.log(1) : console.log(4)
+} else if(a<0) {
+    b>0 ? console.log(2) : console.log(3)
+}
+process.exit();
+});
+
+//node.js에서 쓰이는 fs모듈과 readline모듈에 대해 더 알아봐야겠다.
